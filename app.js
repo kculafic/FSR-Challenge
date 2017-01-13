8c5ef3e9296687f92e7c7dd3754700ca3ea7ffbd
kculafic <kculafic@gmail.com>
2017-01-13 15:24:25 -0800

    First test case functional

--- app.js
+++ app.js
@@ -9,19 +9,45 @@ function handleFileSelect(evt) {
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
+var client = document.getElementById('display');
+
+let stringToSend = '';
 function sortAndSend(stuff) {
   // client.appendChild(data);
-  var client = document.getElementById('display');
   console.log(client);
-  client.insertAdjacentHTML('afterbegin', stuff);
   console.log(stuff);
+  // console.log(json[0].content);
+  // let hello = json[0].content;
+  // let tagged = (`<${hello.tag}> ${hello.content} </${hello.tag}>`);
 
-  console.log(json[0].content);
-  let hello = json[0].content;
-  let tagged = (`<${hello.tag}> ${hello.content} </${hello.tag}>`);
+  stuff.forEach(function(thing) {
+    if (typeof thing.content.content === 'string') {
+      stringToSend += `<${thing.tag}> <${thing.content.tag}> ${thing.content.content} </${thing.content.tag}> </${thing.tag}>`;
+      console.log(stringToSend);
+    } else if (Array.isArray(thing.content.content) === 'true') {
 
-  client.insertAdjacentHTML('afterbegin', tagged);
+    }
+  })
+  document.getElementById('display').innerHTML = stringToSend; 
+
+
+
+
+    // if (typeof thing.tag === 'string') {
+    //   stringToSend += `<${thing.tag}> ${sortAndSend([thing.content])} </${thing.tag}>`;
+    // }
+    // else if (typeof thing.content === 'object') {
+    //   stringToSend += `<${thing.content.tag}> ${sortAndSend([thing.content.content])} </${thing.content.tag}>`;
+    // } else if (typeof thing === 'string') {
+    //   stringToSend += `${sortAndSend(thing)}`;
+    // } else {
+    //   console.log(stringToSend);
+    //   return stringToSend;
+    // }
+  // })
 };
+client.insertAdjacentHTML('afterbegin', stringToSend)
+
 
 function handleFileSelect(evt) {
 	var files = evt.target.files; // FileList object
@@ -44,7 +70,8 @@ function handleFileSelect(evt) {
 
         var data = JSON.stringify(json);
 
-        sortAndSend(data);
+        sortAndSend(json);
+        // client.insertAdjacentHTML('afterbegin', sortAndSend(json));
 			}
 		})(f);
 		reader.readAsText(f);
