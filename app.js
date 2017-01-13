1f93b92d33bc46332cdda86535655d6611b78107
kculafic <kculafic@gmail.com>
2017-01-13 09:12:30 -0800

    Clean up directory

--- app.js
+++ app.js
@@ -1,52 +1,64 @@
-// function handleFileSelect(evt) {
-//     var files = evt.target.files; // FileList object
-//     console.log(files);
-//     // files is a FileList of File objects. List some properties.
-//     var output = [];
-//     for (var i = 0, f; f = files[i]; i++) {
-//       output.push('<li><strong>', escape(f.name), '</strong> (', f.type || 'n/a', ') - ',
-//                   f.size, ' bytes, last modified: ',
-//                   f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a',
-//                   '</li>');
-//     }
-//     document.getElementById('list').innerHTML = '<ul>' + output.join('') + '</ul>';
-//   }
-//
-//   document.getElementById('files').addEventListener('change', handleFileSelect, false);
+var json;
 
-  function handleFileSelect(evt) {
-   var files = evt.target.files; // FileList object
-   console.log(files);
-   var thing = FileReader.readAsText(files);
-   thing.onload;
- }
+function handleFileSelect(evt) {
+	var files = evt.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+
+function handleFileSelect(evt) {
+	var files = evt.target.files; // FileList object
+
+	// files is a FileList of File objects. List some properties.
+	var output = [];
+	for (var i = 0, f; f = files[i]; i++) {
+		var reader = new FileReader();
+
+		// Closure to capture the file information.
+		reader.onload = (function (theFile) {
+			return function (e) {
+				console.log('e readAsText = ', e);
+				console.log('e readAsText target = ', e.target);
+        console.log(e.target.result);
+
+        var micCheck = document.getElementById('display');
+        console.log(micCheck);
+
+        json = JSON.parse(e.target.result);
+
+        console.log(json);
+
+        var data = JSON.stringify(json);
+        // micCheck.appendChild(data);
+        micCheck.insertAdjacentHTML('afterbegin', data);
+        console.log(data);
+
+        function js_traverse(o) {
+          var type = typeof o
+          if (type == "object") {
+            for (var key in o) {
+              if (key === 'tag') {
+                console.log(Object.values(key));
+              }
+              console.log("key: ", key)
+              js_traverse(o[key])
+            }
+          } else {
+            console.log(o)
+          }
+        }
+
+        js_traverse(data);
 
 
-   // Loop through the FileList and render image files as thumbnails.
-  //  for (var i = 0, f; f = files[i]; i++) {
+			}
+		})(f);
+		reader.readAsText(f);
+	}
 
-     // Only process image files.
- //     if (!f.type.match('application/json')) {
- //       continue;
- //     }
- //
- //     var reader = new FileReader();
- //
- //     // Closure to capture the file information.
- //     reader.onload = (function(theFile) {
- //       return function(e) {
- //         // Render thumbnail.
- //        //  var span = document.createElement('span');
- //        //  span.innerHTML = ['<img class="thumb" src="', e.target.result,
- //        //                    '" title="', escape(theFile.name), '"/>'].join('');
- //        //  document.getElementById('list').insertBefore(span, null);
- //        console.log(e);
- //       };
- //     })(f);
- //
- //     // Read in the image file as a data URL.
- //     reader.readAsArrayBuffer(f);
- //   }
- // }
- //
- // document.getElementById('files').addEventListener('change', handleFileSelect, false);
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
