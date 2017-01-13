f39d2db27ed8f4a07efb7ec8afc2a5e47562a949
kculafic <kculafic@gmail.com>
2017-01-13 11:59:34 -0800

    Break out functions from being nested within reader.onload

--- app.js
+++ app.js
@@ -9,6 +9,20 @@ function handleFileSelect(evt) {
 
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
+function sortAndSend(stuff) {
+  // client.appendChild(data);
+  var client = document.getElementById('display');
+  console.log(client);
+  client.insertAdjacentHTML('afterbegin', stuff);
+  console.log(stuff);
+
+  console.log(json[0].content);
+  let hello = json[0].content;
+  let tagged = (`<${hello.tag}> ${hello.content} </${hello.tag}>`);
+
+  client.insertAdjacentHTML('afterbegin', tagged);
+};
+
 function handleFileSelect(evt) {
 	var files = evt.target.files; // FileList object
 
@@ -24,24 +38,13 @@ function handleFileSelect(evt) {
 				console.log('e readAsText target = ', e.target);
         console.log(e.target.result);
 
-        var client = document.getElementById('display');
-        console.log(client);
-
         json = JSON.parse(e.target.result);
 
         console.log(json);
 
         var data = JSON.stringify(json);
-        // client.appendChild(data);
-        client.insertAdjacentHTML('afterbegin', data);
-        console.log(data);
 
-        console.log(json[0].content);
-        let hello = json[0].content;
-        let tagged = (`<${hello.tag}> ${hello.content} </${hello.tag}>`);
-
-        client.insertAdjacentHTML('afterbegin', tagged);
-
+        sortAndSend(data);
 			}
 		})(f);
 		reader.readAsText(f);
